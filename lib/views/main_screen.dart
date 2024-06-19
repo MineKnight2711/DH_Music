176534e9b175087b8d56af753a3ce78577159596
MineKnight2711 <133186183+MineKnight2711@users.noreply.github.com>
2024-06-19 20:49:56 +0700

    popup option

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dh_music/config/config_ex.dart';
 import 'package:dh_music/views/main_love_list_views.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controller/main_screen_controller.dart';
@@ -16,13 +18,17 @@ class MainScreen extends GetView<MainScreenController> {
       ),
       body: Container(
         decoration: const BoxDecoration(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(25),
+          ),
+        ),
         child: TabBarView(
           controller: controller.tabController,
           children: const [
             Center(child: Text("Spotify")),
             Center(child: LoveListView()),
-            Center(child: Text("Danh sách phát")),
+            PlaylistView(),
             Center(child: Text("Bài hát")),
             Center(child: Text("Album")),
             Center(child: Text("Nghệ sĩ")),
@@ -30,6 +36,75 @@ class MainScreen extends GetView<MainScreenController> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class PlaylistView extends StatelessWidget {
+  const PlaylistView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        PopupMenuButton(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem(
+                child: Text(
+                  "Thứ tự t.chỉnh",
+                  style: AppFonts.openSans(
+                    fontSize: AppFontSizes.size12,
+                  ),
+                ),
+              ),
+              PopupMenuItem(
+                child: Text(
+                  "Tên",
+                  style: AppFonts.openSans(
+                    fontSize: AppFontSizes.size12,
+                  ),
+                ),
+              ),
+              PopupMenuItem(
+                child: Text(
+                  "Ngày thêm",
+                  style: AppFonts.openSans(
+                    fontSize: AppFontSizes.size12,
+                  ),
+                ),
+              ),
+              PopupMenuItem(
+                child: Text(
+                  "Ngày đã phát",
+                  style: AppFonts.openSans(
+                    fontSize: AppFontSizes.size12,
+                  ),
+                ),
+              ),
+            ];
+          },
+          // icon: const Icon(CupertinoIcons.sort_down),
+          splashRadius: 0,
+
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(CupertinoIcons.sort_down),
+                Text("Sắp xếp theo: Thứ tự tùy chỉnh"),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
