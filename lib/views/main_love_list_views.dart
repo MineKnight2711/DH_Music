c2466caec65c1635bda30bd3fd340e23c9d1586f
Huỳnh Văn Hiệp <hiep.huynh@mekongnet.vn>
2024-06-19 19:50:14 +0700

    love list

--- lib/views/main_love_list_views.dart
+++ lib/views/main_love_list_views.dart
@@ -13,19 +13,45 @@ class LoveListView extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "Không có yêu thích",
+            "Danh sách yêu thích",
             style: AppFonts.openSans(
               fontSize: AppFontSizes.size20,
               color: AppColors.samsungIconGrey,
             ),
           ),
           const SizedBox(height: 20),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              children: List.generate(3, (index) => _buildSongTile(index)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSongTile(int index) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: Image.asset('assets/images/love_list.jpg').image,
+          opacity: 0.5,
+        ),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10.0),
           Text(
-            "Hãy chạm vào biểu tượng trái tim bên cạnh danh sách phát, album, nghệ sĩ và thư mục yêu thích của bạn và chúng sẽ xuất hiện ở đây.",
-            textAlign: TextAlign.center,
+            "Bài hát yêu thích",
             style: AppFonts.openSans(
-              fontSize: AppFontSizes.size12,
-              color: AppColors.samsungIconGrey,
+              fontSize: AppFontSizes.size16,
+              color: Colors.white,
             ),
           ),
         ],
